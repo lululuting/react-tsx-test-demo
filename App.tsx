3c4fd5040c47204002d22e783c326a7f6d6ce655
lululuting <474628778@qq.com>
2022-08-15 13:37:00 +0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -3,12 +3,31 @@ import Demo, { Ulist } from './demo';
 import './style.css';
 
 const App: React.FC = () => {
+  const renderNoMoreDataTips = () => {
+    return <div style={{ color: 'blue' }}>就是没有了</div>;
+  };
+  const renderItem = (item) => {
+    return (
+      <div style={{ borderBottom: '1px solid #333', padding: 10 }}>
+        <span style={{ color: 'block' }}>{item.key}</span>
+        ---
+        <span style={{ color: 'red' }}>{item.value}</span>;
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Ulist />
-      <Demo />
+      <h1>通用分页组件</h1>
+      {/* <Ulist /> */}
+
+      <Demo
+        noMoreDataTips={renderNoMoreDataTips}
+        renderItem={renderItem}
+        mode={'paging'}
+        limit={9}
+        // noDataFn={(res) => res.length >= 20}
+      />
     </div>
   );
 };
--- demo.tsx
+++ demo.tsx
@@ -1,34 +1,71 @@
-import React, { useState, useEffect, Component } from 'react';
-// 函数组件
+import * as React from 'react';
 
 /**定义函数组件父传子 属性值类型。问号：该属性可有可无
 接口的声明不要放在import语句的前面，可能会报错**/
-interface PropsFC {
-  queryPromise?: any,
-  loading?: boolean,
-  page?: number,
-  limit?:  number,
-  noDataFn?: any,
-  noDataTips?: any,
-  loadMore?: boolean,
-  noMoreDataTips?: any,
-  children?: any,
-}
-// FC:function component 函数组件 
-const Demo:React.FC<PropsFC> = (props) => {
-  // 格式 const [状态变量,修改状态的方法]=useState<变量的类型>(变量的值)
-  const [list, setList] = useState<any[]>([]);
-  const [page, setPage] = useState<number>(props.page || 1);
-  const [limit, setLimit] = useState<number>(props.limit || 10);
-  const [loading, setLoading] = useState<boolean>(props.loading || false);
-  const [isNoMoreData, setIsNoMoreData] = useState<boolean>(false);
+type Key = string | number;
 
-  useEffect(() => {
+// 该类型的变量值只能是两种：null 和 ReactElement实例。通常情况下，函数组件返回ReactElement（JXS.Element）的值。
+interface ReactElement<
+  P = any,
+  T extends string | React.JSXElementConstructor<any> =
+    | string
+    | React.JSXElementConstructor<any>
+> {
+  type: T;
+  props: P;
+  key: Key | null;
+}
+type ReactText = string | number;
+type ReactChild = ReactElement | ReactText;
+interface ReactNodeArray extends Array<ReactNode> {}
+type ReactFragment = {} | ReactNodeArray;
+interface ReactPortal extends ReactElement {
+  key: Key | null;
+  children: ReactNode;
+}
+type ReactNode =
+  | ReactChild
+  | ReactFragment
+  | ReactPortal
+  | boolean
+  | null
+  | undefined;
+
+interface PropsFC {
+  mode?: 'paging' | 'infinite'; // 枚举
+  page?: number;
+  limit?: number;
+  loadMore?: boolean;
+  children?: any;
+  // 方法 ------------
+  queryPromise?: string | (() => any);
+  noDataFn?: (res?: any[], list?: any[]) => boolean;
+  noDataTips?: () => string | (() => any);
+  noMoreDataTips?: ReactNode;
+  renderItem?: (item?: {}, index?: number, list?: any[]) => any;
+}
+
+interface ItemType {
+  key: number | string;
+  value: string;
+}
+
+// FC:function component 函数组件
+const Demo: React.FC<PropsFC> = (props) => {
+  // 格式 const [状态变量,修改状态的方法]=useState<变量的类型>(变量的值)
+  const [list, setList] = React.useState<any[]>([]);
+  const [page, setPage] = React.useState<number>(props.page || 1);
+  const [limit, setLimit] = React.useState<number>(props.limit || 10);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [isNoMoreData, setIsNoMoreData] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
     getListData();
   }, []);
 
   const getListData = async () => {
-    setPage((prev) => prev + 1);
+    setPage(page + 1);
+
     let data = await getListDataPromise();
     setList([...list, ...data]);
     if (!data.length) {
@@ -38,14 +75,21 @@ const Demo:React.FC<PropsFC> = (props) => {
 
   const getListDataPromise = async () => {
     setLoading(true);
+    let api: string = '默认的api地址';
 
     if (props.queryPromise) {
-      return props.queryPromise().finally(() => {
-        setLoading(false);
-      });
+      if (typeof props.queryPromise === 'function') {
+        return props.queryPromise().finally(() => {
+          setLoading(false);
+        });
+      }
+      // api路径
+      api = props.queryPromise;
     }
 
-    let data: any[]  = [];
+    // 如果没有传入queryPromise 走默认
+
+    let data: Array<ItemType> = [];
     for (let i = 0; i < limit; i++) {
       data.push({
         key: page + '_' + i + 'key',
@@ -54,8 +98,9 @@ const Demo:React.FC<PropsFC> = (props) => {
     }
 
     return new Promise((req, rej) => {
+      console.log('api：' + api);
       setTimeout(() => {
-        if (props.noDataFn ? props.noDataFn() : list.length >= 40) {
+        if (props.noDataFn ? props.noDataFn(data, list) : list.length >= 20) {
           req([]);
         } else {
           req(data);
@@ -66,11 +111,25 @@ const Demo:React.FC<PropsFC> = (props) => {
     });
   };
 
+  const renderNoMoreData = (): any => {
+    if (typeof props.noMoreDataTips === 'function') {
+      return props.noMoreDataTips();
+    } else {
+      return props.noMoreDataTips || '没有更多数据了';
+    }
+  };
+
   return (
     <div>
       <div style={{ position: 'relative' }}>
         {list.length
-          ? list.map((item) => <div key={item.key}>{item.value}</div>)
+          ? list.map((item, index) => {
+              // 自定义renderItem
+              if (props.renderItem) {
+                return props.renderItem(item, index, list);
+              }
+              return <div key={item.key}>{item.value}</div>;
+            })
           : props.noDataTips || '没有数据'}
         {props.children}
 
@@ -88,59 +147,60 @@ const Demo:React.FC<PropsFC> = (props) => {
             loading
           </div>
         ) : null}
-      </div>
 
+        {/* 常规分页 */}
+
+        {/* {
+          
+        } */}
+      </div>
       {props.loadMore ? (
         !isNoMoreData ? (
           <button onClick={getListData} disabled={loading}>
             加载更多
           </button>
-        ) : props.noMoreDataTips ? (
-          props.noMoreDataTips
         ) : (
-          '没有更多数据了'
+          renderNoMoreData()
         )
       ) : null}
     </div>
   );
-}
+};
 
 //定义props的默认值
-Demo.defaultProps={
-  loadMore: true
-}
-
+Demo.defaultProps = {
+  loadMore: true,
+};
+export default Demo;
 
 // ------------------------------------------------------------------------------------------------
 // 类组件
 interface Props {
-  color?: string,
+  color?: string;
 }
 
 //定义state中键的类型
-interface States{
-  msg: number,
-  age: number
+interface States {
+  msg: number;
+  age: number;
 }
+
 /**声明一个类组件 Component<定义props的数据类型,定义state的数据类型>**/
-export class Ulist extends Component<Props, States> {
+export class Ulist extends React.Component<Props, States> {
   //定义props的默认值
   static defaultProps = {
-    color: 'red'
-  }
+    color: 'red',
+  };
   state = {
     msg: 123,
-    age: 10
-  }
+    age: 10,
+  };
   render() {
-    let { color } =this.props
+    let { color } = this.props;
     return (
-      <div style={{color}}>
+      <div style={{ color }}>
         msg:{this.state.msg}, age: {this.state.age},
       </div>
-    )
+    );
   }
 }
-
-
-export default Demo;
